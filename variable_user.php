fc7b71e2ef8fe3c0e12c2006e3914348df869f37
Rafi <92348196+Rafi2705@users.noreply.github.com>
2022-02-28 07:03:10 +0700

    Add files via upload

---
+++ variable_user.php
@@ -0,0 +1,13 @@
+<?php
+ // definisikan variables
+ $nama = 'Rafi Yulian';
+ $umur = 18;
+ $berat = 65.5;
+
+ echo 'Nama : ' . $nama;
+ echo '<br/>Umur : ' . $umur.' Tahun';
+ echo '<br/>Berat : '.$berat.' Kg';
+
+ echo "<br/>Hello $nama Apakabar";
+
+ ?>
